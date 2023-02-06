625247a8c12c8cd680dca074e0f6d3b1dc432451
kabeya <kabeya@cabinetwork.jp>
2023-02-06 10:58:20 +0900

    Initial Commit

---
+++ SwiftUIImageCropPicker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIImageCropPicker
+//
+//  Created by Takenori Kabeya on 2023/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIImageCropPicker/SwiftUIImageCropPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageCropPickerApp.swift
+//  SwiftUIImageCropPicker
+//
+//  Created by Takenori Kabeya on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageCropPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
